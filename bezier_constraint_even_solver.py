08865aa0e5357b5e41dda77a95d47927ffa33ecf
songrun <songruner@gmail.com>
2014-01-09 18:10:44 -0500

    change constraint name

--- bezier_constraint_even_solver.py
+++ bezier_constraint_even_solver.py
@@ -233,7 +233,7 @@ class BezierConstraintSolverEven( BezierConstraintSolver ):
 		elif smoothness == 'C1': num = 4       ## C1
 		elif smoothness == 'G1': num = 2       ## G1
 
-		if fixed != 0:
+		if fixed == 'True' or fixed == True:
 			num += 2
 	
 		return num	
